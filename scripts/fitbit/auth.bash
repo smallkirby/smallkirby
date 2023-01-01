136def6696ff89d9fd631267bec28b21fbfbafde
smallkirby <ssmallkirby@gmail.com>
2023-01-01 10:11:54 +0900

    init fitbit fetcher
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

---
+++ scripts/fitbit/auth.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+export C_RST="\e[0m"
+export C_GREEN="\e[32m"
+export C_LGREEN="\e[92m"
+export C_RED="\e[31m"
+export C_PURPLE="\e[35m"
+export C_BOLD="\e[1m"
+export C_UL="\e[4m"
+
+echo -en "${C_GREEN}[+]${C_RST} Checking env...: "
+for e in \
+  "CLIENT_ID" \
+  "CLIENT_SECRET" \
+; do
+  if [ -z "$(eval echo \$"$e")" ]; then
+    echo -e "\n${C_RED}[!]${C_RST} Env $e not found"
+    exit 1
+  fi
+done
+echo -e "${C_GREEN}OK${C_RST}"
+
+VERIFIER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 100 | head -n 1)
+CHALLENGE=$(echo -n $VERIFIER | openssl dgst -sha256 -binary | openssl base64 | tr -d '=' | tr '/+' '_-')
+
+xdg-open \
+"https://www.fitbit.com/oauth2/authorize?client_id=$CLIENT_ID&response_type=code&code_challenge=$CHALLENGE&code_challenge_method=S256&scope=sleep" \
+> /dev/null 2>&1
+
+echo -en "${C_GREEN}[+]${C_RST} Waiting for code > "
+read CODE <&0
+
+curl https://api.fitbit.com/oauth2/token \
+  -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -H "Authorization: Basic $(echo -n "$CLIENT_ID:$CLIENT_SECRET" | base64)" \
+  -d "client_id=$CLIENT_ID" \
+  -d "grant_type=authorization_code" \
+  -d "code=$CODE" \
+  -d "code_verifier=$VERIFIER"
---
+++ scripts/fitbit/index.ts
@@ -0,0 +1,121 @@
+import { AuthorizationCode } from 'simple-oauth2';
+import * as dotenv from 'dotenv';
+import * as admin from 'firebase-admin';
+import axios from 'axios';
+import dayjs, { Dayjs } from 'dayjs';
+
+type SleepLog = {
+  dateOfSleep: Dayjs,
+  endTime: Dayjs,
+  startTime: Dayjs,
+  isMainSleep: boolean,
+  timeInBed: number,
+  efficiency: number,
+};
+
+const toSleepLog = (obj: any): SleepLog => ({
+  dateOfSleep: dayjs(obj.dateOfSleep),
+  endTime: dayjs(obj.endTime),
+  startTime: dayjs(obj.startTime),
+  isMainSleep: obj.isMainSleep,
+  timeInBed: obj.timeInBed,
+  efficiency: obj.efficiency,
+});
+
+dotenv.config();
+dayjs.locale('jp');
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const FIREBASE_SA_B64 = process.env.FIREBASE_SA_BASE64 || '';
+
+const EXPIRATION_WINDOW_IN_SECONDS = 300;
+
+if (!CLIENT_ID || !CLIENT_SECRET || !FIREBASE_SA_B64) {
+  throw new Error('CLIENT_ID or CLIENT_SECRET or FIREBASE_SA_BASE64 is not defined');
+}
+
+// init Firebase
+const FIREBASE_KEY = Buffer.from(FIREBASE_SA_B64, 'base64').toString('ascii');
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(FIREBASE_KEY)),
+});
+
+// init OAuth client
+const client = new AuthorizationCode({
+	client: {
+		id: CLIENT_ID,
+		secret: CLIENT_SECRET,
+	},
+	auth: {
+		tokenHost: 'https://api.fitbit.com',
+		tokenPath: '/oauth2/token',
+		authorizeHost: 'https://www.fitbit.com',
+		authorizePath: '/oauth2/authorize',
+	},
+});
+
+const main = async () => {
+  // Restore saved tokens
+  const tokenSnap = await admin.firestore().doc('fitbit/tokens').get();
+  if (!tokenSnap.exists) {
+    console.error('No tokens found in Firestore');
+    process.exit(1);
+  }
+
+  // Check expiration
+  const tokenData = tokenSnap.data()!;
+  tokenData.expires_at = tokenData.expires_at.toDate();
+  const userId = tokenData.user_id as string;
+  const token = client.createToken(tokenData);
+
+  if (token.expired(EXPIRATION_WINDOW_IN_SECONDS)) {
+    console.log('Token expired, refreshing...');
+    const newToken = await token.refresh();
+    await admin.firestore().doc('fitbit/tokens').set(newToken.token);
+    console.log('Token refreshed.');
+  }
+
+  console.log('Fetching sleep log...');
+  const sleep = await axios.get(`https://api.fitbit.com/1.2/user/${userId}/sleep/date/2023-01-01/2023-01-31.json`, {
+    headers: {
+      Authorization: `Bearer ${token.token.access_token}`,
+      Accept: 'application/json',
+      AcceptLocale: 'ja_JP',
+    },
+  });
+
+  const logs: SleepLog[] = sleep.data.sleep.map(toSleepLog);
+
+  let totalDays = 0;
+  let earlyDaysCount = 0;
+  const today = dayjs();
+  let cur = dayjs('2023-01-01');
+  while (cur.year() === 2023) {
+    let todaysLogs = logs
+      .filter((l) => l.dateOfSleep.isSame(cur, 'day'))
+      .sort((a, b) => a.startTime.diff(b.startTime));
+    if (todaysLogs.length !== 0) {
+      let log = todaysLogs.find((l) => l.isMainSleep);
+      if (!log) {
+        log = todaysLogs[todaysLogs.length - 1];
+      }
+
+      if (log.endTime.hour() < 9) {
+        ++earlyDaysCount;
+      }
+    }
+
+    if (cur.isSame(today, 'day')) {
+      ++totalDays;
+    }
+    cur = cur.add(1, 'day');
+  }
+
+  console.log(`${earlyDaysCount} / ${totalDays}`);
+};
+
+(async () => {
+  await main();
+})();
+
